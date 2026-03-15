4a322d8b75c01c4e6ffecd0ad479f9b28c08564e
Yiyoung Liu <yiyoung.md@gmail.com>
2026-03-15 00:28:50 -0400

    feat: developer environment setup
    will write documentation later

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default defineConfig([
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        plugins: {
+            prettier,
+        },
+        rules: {
+            "prettier/prettier": "warn",
+            "no-constructor-return": "error",
+            "no-self-compare": "error",
+            "no-template-curly-in-string": "warn",
+            "eqeqeq": "error",
+            "no-eval": "error",
+            "no-floating-decimal": "error",
+            "no-implicit-coercion": [
+                "warn",
+                {
+                    allow: ["!!"]
+                }
+            ],
+            "no-var": "error",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_"
+                }
+            ],
+            "prefer-const": "warn",
+            "prefer-rest-params": "warn",
+            "yoda": ["error", "never"]
+        }
+    }
+]);
