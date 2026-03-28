a7b9fa3bfa9d1bc194a56a58ca061a458fed8e5f
thesuperRL <89622221+thesuperRL@users.noreply.github.com>
2026-03-28 18:12:36 -0400

    feat: flake (#20)

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "CMU Housing";
+
+  nixConfig = {
+    extra-substituters = [ "https://scottylabs.cachix.org" ];
+    extra-trusted-public-keys = [
+      "scottylabs.cachix.org-1:hajjEX5SLi/Y7yYloiXTt2IOr3towcTGRhMh1vu6Tjg="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system: {
+        devenv = devenv.packages.${system}.devenv;
+      });
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                languages.javascript.enable = true;
+                languages.javascript.npm.enable = true;
+              }
+            ];
+          };
+        }
+      );
+    };
+}
